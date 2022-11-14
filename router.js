2018e7c8b249d1c438de4ea8892bf9ba1a510071
suyash-88051 <82015482+suyash-88051@users.noreply.github.com>
2022-11-14 14:22:55 +0530

    Update router.js

--- router.js
+++ router.js
@@ -4,13 +4,21 @@ var router = express.Router();
 
 const credential = {
     email: "suyash@gmail.com",
-    password: "1234"
+    password: "1234",
+}
+const credential2 = {
+    email: "harsh@gmail.com",
+    password: "5678"
 }
 
 router.post('/login', (req, res) => {
     if (req.body.email == credential.email && req.body.password == credential.password) {
         req.session.user = req.body.email;
         res.redirect('/route/profile');
+        //res.end("Login Sucessful");
+    } else if (req.body.email == credential2.email && req.body.password == credential2.password) {
+        req.session.user = req.body.email;
+        res.redirect('/route/profile2');
         //res.end("Login Sucessful");
     } else {
         res.end("Invalid Username or password");
@@ -20,6 +28,13 @@ router.post('/login', (req, res) => {
 router.get('/profile', (req, res) => {
     if (req.session.user) {
         res.render('profile', { user: req.session.user })
+    } else {
+        res.send("Unauthorised User");
+    }
+})
+router.get('/profile2', (req, res) => {
+    if (req.session.user) {
+        res.render('profile2', { user: req.session.user })
     } else {
         res.send("Unauthorised User");
     }
